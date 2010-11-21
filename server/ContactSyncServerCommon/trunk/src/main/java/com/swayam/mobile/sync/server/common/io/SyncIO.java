229c08e3fa5dd6e23e0c5d01d78a466941ae611a
paawak <paawak@gmail.com>
2010-11-21 09:24:30 +0000

    git-svn-id: svn://localhost/swayam@788 c64e1095-0faf-4994-8903-70d657f34d2f

---
+++ server/ContactSyncServerCommon/trunk/src/main/java/com/swayam/mobile/sync/server/common/io/SyncIO.java
@@ -0,0 +1,30 @@
+/*
+ * SyncIO.java
+ *
+ * Created on Nov 21, 2010 2:37:30 PM
+ *
+ * Copyright (c) 2002 - 2010 : Swayam Inc.
+ *
+ * P R O P R I E T A R Y & C O N F I D E N T I A L
+ *
+ * The copyright of this document is vested in Swayam Inc. without
+ * whose prior written permission its contents must not be published,
+ * adapted or reproduced in any form or disclosed or
+ * issued to any third party.
+ */
+
+package com.swayam.mobile.sync.server.common.io;
+
+import java.io.InputStream;
+
+/**
+ * 
+ * @author paawak
+ */
+public class SyncIO {
+
+    public void read(InputStream is) {
+
+    }
+
+}
