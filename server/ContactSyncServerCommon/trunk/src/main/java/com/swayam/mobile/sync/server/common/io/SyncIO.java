6d3e4ae08e9d4916d5dd4dba86be737096fd48c8
paawak <paawak@gmail.com>
2010-11-21 10:05:37 +0000

    git-svn-id: svn://localhost/swayam@794 c64e1095-0faf-4994-8903-70d657f34d2f

--- server/ContactSyncServerCommon/trunk/src/main/java/com/swayam/mobile/sync/server/common/io/SyncIO.java
+++ server/ContactSyncServerCommon/trunk/src/main/java/com/swayam/mobile/sync/server/common/io/SyncIO.java
@@ -15,6 +15,8 @@
 
 package com.swayam.mobile.sync.server.common.io;
 
+import java.io.DataInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
@@ -23,7 +25,16 @@ import java.io.InputStream;
  */
 public class SyncIO {
 
-    public void read(InputStream is) {
+    public void read(InputStream is) throws IOException {
+
+        DataInputStream di = new DataInputStream(is);
+        String data = di.readUTF();
+
+        System.out
+                .println("**************************************************************");
+        System.out.println(data);
+        System.out
+                .println("**************************************************************");
 
     }
 
